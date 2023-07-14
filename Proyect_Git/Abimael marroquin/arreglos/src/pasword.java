391bb102811f4a5af0c81a913826c8689b2b19cc
AbimaelMarroquin <135286719+AbimaelMarroquin@users.noreply.github.com>
2023-07-14 09:12:46 -0600

    Add files via upload

---
+++ Proyect_Git/Abimael marroquin/arreglos/src/pasword.java
@@ -0,0 +1,32 @@
+import javax.swing.JOptionPane;
+
+public class pasword {
+
+	public static void main(String[] args) {
+	
+
+		  String password = JOptionPane.showInputDialog(null, "Ingrese la contraseña:");
+		  String letraObjetivoInput = JOptionPane.showInputDialog(null, "Ingrese la letra objetivo:");
+		  char letraObjetivo = letraObjetivoInput.charAt(0);
+	
+		// Convertir la contraseña a un arreglo de caracteres
+	        char[] arregloPassword = password.toCharArray();	
+		  
+	     // Verificar si la letra objetivo está presente en el arreglo
+	        boolean letraEncontrada = false;
+	        for (char A : arregloPassword) {
+	            if (A== letraObjetivo) {
+	                letraEncontrada = true;
+	                break;
+	            }
+	        }
+	                // Mostrar el resultado utilizando JOptionPane
+	                if (letraEncontrada) {
+	                    JOptionPane.showMessageDialog(null, "La contraseña contiene la letra objetivo.");
+	                } else {
+	                    JOptionPane.showMessageDialog(null, "La contraseña no contiene la letra objetivo.");
+	            }
+	        }
+	        }
+
+
